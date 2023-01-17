aefdaaf6bb27c9f1a4889515ee6121f5c3e184ef
Marçal <marcal.asbo@gmail.com>
2023-01-17 12:21:24 +0100

    6.4 Rutas básicas de navegación

--- src/main/kotlin/com/maasbodev/plugins/Routing.kt
+++ src/main/kotlin/com/maasbodev/plugins/Routing.kt
@@ -1,16 +1,49 @@
 package com.maasbodev.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.html.respondHtml
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import kotlinx.html.a
+import kotlinx.html.body
+import kotlinx.html.div
+import kotlinx.html.h1
+import kotlinx.html.head
+import kotlinx.html.p
+import kotlinx.html.title
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/html") {
+            call.respondHtml(status = HttpStatusCode.OK) {
+                head {
+                    title { +"Hello Ktor" }
+                }
+                body {
+                    h1 { +"This is an h1" }
+                    p { +"This is a paragraph" }
+                    div {
+                        a(href = "html/clicked") { +"Click me" }
+                    }
+                }
+            }
+        }
+        get("/html/clicked") {
+            call.respondHtml(status = HttpStatusCode.OK) {
+                head {
+                    title { +"Clicked" }
+                }
+                body {
+                    h1 { +"Button Clicked" }
+                }
+            }
         }
     }
 }
